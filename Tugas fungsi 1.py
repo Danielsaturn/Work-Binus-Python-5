45c822827ec885f2ff63f214bc7bc9c0069f8369
Danielsaturn <danielbussiness2000@gmail.com>
2024-12-14 14:50:08 +0700

    Update Tugas fungsi 1.py

--- Tugas fungsi 1.py
+++ Tugas fungsi 1.py
@@ -3,12 +3,28 @@ def introduction():
 | DANIEL     SATRIA |
 |       Cinere      |
 ---------------------""")
+#this definition is for introducinfg the creator of this code
 
 def addition(value1, value2):
     
     addre= value1 + value2
     
     print(addre)
+#this definition is for addition
+
+def subtraction(value1, value2):
+    
+    addre= value1 - value2
+    
+    print(addre)
+#this definition isfor subtraction
+
+def division(value1, value2):
+    
+    addre= value1 / value2
+    
+    print(addre)
+#this definition is for division
 
 introduction()
 arithch = str(input("Enter Menu (+|-|/|*|%|stop): "))
@@ -18,3 +34,12 @@ value2 = float(input("Please enter the second value: "))
 if (arithch == "+"):
     
     addition(value1, value2)
+    
+elif (arithch == "-"):
+    
+    subtraction(value1, value2)
+    
+elif (arithch == "/"):
+    
+    division(value1, value2)
+    
